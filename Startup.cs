b795f3f505fbcaa96c56783febaf394b4e8aa5d2
gabelogue18 <LogueGA@alfredstate.edu>
2017-02-27 09:21:58 -0500

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DatabaseConcepts3.Startup))]
+namespace DatabaseConcepts3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
